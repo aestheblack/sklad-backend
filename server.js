a6d62480d7a63f4a8edd2eea9433bd276ccf2ed3
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-11-25 19:17:41 +0500

    Add health check endpoint to server and organize port declaration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
 const router = require("./routes/router.js");
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
+app.get("/", (req, res) => {
+  return res.json({ message: "API server is running!" });
+});
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
